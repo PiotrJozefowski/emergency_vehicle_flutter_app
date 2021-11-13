eb3a2ebac51a48dcdfd3424fdb1a919bd2b5397a
piotr_jozefowski <piotr.jozefowski@certusoft.pl>
2021-11-13 15:49:27 +0100

    Basic structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,14 @@
+import 'package:emergency_vehicle_flutter_app/screens/authenticate/authenticate.dart';
+import 'package:emergency_vehicle_flutter_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    //return Home or Auth widger
+    return Home();
+  }
+}
