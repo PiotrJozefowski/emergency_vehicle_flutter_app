41031b05e05005cfd65129363bca96c623df40bc
piotr_jozefowski <piotr.jozefowski@certusoft.pl>
2021-11-21 17:21:21 +0100

    2nd version - initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
-import 'package:emergency_vehicle_flutter_app/screens/wrapper.dart';
+import 'package:emergency_vehicle_flutter_app/log_in/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: Wrapper(),
+    return FutureBuilder(
+      future: _initialization,
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(
+                  child: Center(
+                    child: Text('Welcome')
+                  )
+                )
+              )
+            );
+          }else if(snapshot.hasError){
+            return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                    body: Center(
+                        child: Center(
+                            child: Text('Please wait')
+                        )
+                    )
+                )
+            );
+          }
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.lightGreen,
+            ),
+            home: LoginScreen()
+          );
+        }
     );
   }
 }
--- lib/models/userf.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class UserF{
-
-  final String uid;
-
-  UserF({required this.uid});
-}
-
